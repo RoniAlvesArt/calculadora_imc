7f2c827e36fbe40c16240741543b22fad300ed15
Roni <ronialvesart@gmail.com>
2023-10-21 22:27:25 -0300

    Atualizando com entrada de dados

--- bin/calculadora_imc.dart
+++ bin/calculadora_imc.dart
@@ -1,5 +1,19 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:calculadora_imc/calculadora_imc.dart' as calculadora_imc;
 
 void main(List<String> arguments) {
-  print('Hello world: ${calculadora_imc.calculate()}!');
+  print("Digite seu peso:");
+  var line = stdin.readLineSync(encoding: utf8);
+  try{
+      double numero = double.parse(line ?? "");
+        print(numero);
+  } catch(e){
+    print("Número ($line) inválido! Digite um número para continuar:");
+  }finally{
+    print("Aplicação executada com sucesso.");
+  }
+
+
 }
